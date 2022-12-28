5e4fc10eb8602b35ed0bc8cdc336b51deae514e5
SMAnas-00 <gouheranas4456@gmail.com>
2022-12-28 16:38:57 +0500

    Updates

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -1,5 +1,7 @@
 // ignore_for_file: file_names, prefer_const_constructors, prefer_const_literals_to_create_immutables
 
+import 'package:carousel_slider/carousel_controller.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
 import 'package:palmer/addons/NavBar.dart';
@@ -25,8 +27,21 @@ class _HomeState extends State<Home> {
   }
 }
 
-class MyHome extends StatelessWidget {
-  const MyHome({super.key});
+class MyHome extends StatefulWidget {
+  @override
+  State<MyHome> createState() => _MyHomeState();
+}
+
+class _MyHomeState extends State<MyHome> {
+  List Sliderimglist = [
+    {"id": 1, "image_path": 'images/img_slider1.jpg'},
+    {"id": 2, "image_path": 'images/img_slider2.jpg'},
+    {"id": 3, "image_path": 'images/img_slider3.jpg'}
+  ];
+
+  final CarouselController carouselControl = CarouselController();
+
+  int currentindex = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -40,15 +55,15 @@ class MyHome extends StatelessWidget {
           ),
           backgroundColor: Color.fromARGB(255, 254, 253, 252),
           actions: [
-            GestureDetector(
-              child: Container(
-                margin: EdgeInsets.symmetric(horizontal: 15),
-                child: Icon(Icons.search),
-              ),
-              onTap: () {
-                showSearch(context: context, delegate: CustomSearchDelegate());
-              },
-            ),
+            // GestureDetector(
+            //   child: Container(
+            //     margin: EdgeInsets.symmetric(horizontal: 15),
+            //     child: Icon(Icons.search),
+            //   ),
+            //   onTap: () {
+            //     showSearch(context: context, delegate: CustomSearchDelegate());
+            //   },
+            // ),
             GestureDetector(
               child: Container(
                 margin: EdgeInsets.fromLTRB(0, 0, 2, 0),
@@ -65,101 +80,81 @@ class MyHome extends StatelessWidget {
         body: Container(
           child: Column(
             children: [
-              Center(
-                child: Container(
-                  child: Image.asset(
-                    'images/image07.jpg',
-                    height: 200.0,
-                    width: 500.0,
-                    fit: BoxFit.cover,
+              // SLider Working =================
+              Stack(
+                children: [
+                  InkWell(
+                    onTap: () {
+                      print(currentindex);
+                    },
+                    child: CarouselSlider(
+                      items: Sliderimglist.map((item) => Image.asset(
+                            item['image_path'],
+                            fit: BoxFit.fill,
+                            width: double.infinity,
+                          )).toList(),
+                      carouselController: carouselControl,
+                      options: CarouselOptions(
+                        scrollPhysics: const BouncingScrollPhysics(),
+                        autoPlay: true,
+                        autoPlayAnimationDuration: Duration(milliseconds: 2000),
+                        enableInfiniteScroll: true,
+                        aspectRatio: 2.5,
+                        viewportFraction: 1,
+                        onPageChanged: (index, reason) {
+                          setState(() {
+                            currentindex = index;
+                          });
+                        },
+                      ),
+                    ),
                   ),
-                ),
+                  Positioned(
+                      bottom: 10.0,
+                      left: 0,
+                      right: 0,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: Sliderimglist.asMap().entries.map((entry) {
+                          // print(entry);
+                          // print(entry.key);
+                          return GestureDetector(
+                            onTap: () =>
+                                carouselControl.animateToPage(entry.key),
+                            child: Container(
+                              width: currentindex == entry.key ? 17 : 7,
+                              height: 7.0,
+                              margin: const EdgeInsets.symmetric(
+                                horizontal: 3.0,
+                              ),
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10),
+                                  color: currentindex == entry.key
+                                      ? Color.fromARGB(255, 255, 194, 101)
+                                      : Colors.teal),
+                            ),
+                          );
+                        }).toList(),
+                      )),
+                ],
               ),
+
               SizedBox(
                 height: 60.0,
               ),
-              // Center(
-              //   child: Container(
-              //     margin: EdgeInsets.fromLTRB(50.0, 25.0, 50.0, 15.0),
-              //     padding: EdgeInsets.all(2.0),
-              //     child: Text(
-              //       'kmcmadkmcmsdmckamlkmslcmslckmclksmcslkmckmscmalskmakcmkmakmclkmsd/n mcoalscmsamlasmal',
-              //       style: TextStyle(
-              //           fontSize: 15.0, fontWeight: FontWeight.normal),
-              //     ),
-              //   ),
-              // )
 
               //     --------------------- Home page Work -------------------------------------
-              Container(
-                margin: EdgeInsets.fromLTRB(50.0, 0, 50.0, 5.0),
-                child: Row(
-                  children: [
-                    Container(
-                      margin: EdgeInsets.only(left: 20.0),
-                      padding: EdgeInsets.all(10.0),
-                      decoration: BoxDecoration(
-                        gradient: LinearGradient(
-                            colors: [
-                              Colors.amberAccent,
-                              Color.fromARGB(255, 104, 99, 80)
-                            ],
-                            begin: Alignment.topCenter,
-                            end: Alignment.bottomCenter),
-                      ),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [Icon(Icons.home), Text('Home')],
-                      ),
-                    ),
-                    Container(
-                      margin: EdgeInsets.only(left: 10.0),
-                      padding: EdgeInsets.all(10.0),
-                      decoration: BoxDecoration(
-                        gradient: LinearGradient(
-                            colors: [
-                              Colors.amberAccent,
-                              Color.fromARGB(255, 104, 99, 80)
-                            ],
-                            begin: Alignment.topCenter,
-                            end: Alignment.bottomCenter),
-                      ),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Icon(Icons.mosque_outlined),
-                          Text('UMMRAHH')
-                        ],
-                      ),
-                    ),
-                    Container(
-                      child: Column(
-                        children: [],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Container(
-                child: Row(
-                  children: [
-                    Container(
-                      child: Column(
-                        children: [],
-                      ),
-                    ),
-                    Container(
-                      child: Column(
-                        children: [],
-                      ),
-                    ),
-                    Container(
-                      child: Column(
-                        children: [],
-                      ),
-                    ),
-                  ],
-                ),
+
+              Row(
+                children: [
+                  Container(
+                      child: Image.network(
+                    'https://cdn-icons-png.flaticon.com/512/424/424162.png',
+                    width: 0.0,
+                    height: 30.0,
+                  )),
+                  Container(),
+                ],
               ),
             ],
           ),
